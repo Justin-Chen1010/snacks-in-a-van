2228c553422299da252acad6394aa71b5faa3e0c
Jonathan Jauhari <jonjau3q@gmail.com>
2021-04-09 15:13:13 +1000

    added schemas

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const app = express();
 
 // https://github.com/INFO30005-2021-SM1/project-t12-roboto
 app.use(express.urlencoded({ extended: false }));
-// TODO: dont forget to include image
-ITEMS = [
-    {
-        name: "Cappuccino",
-        price: 3.8
-    },
-    {
-        name: "Latte",
-        price: 4.5
-    },
-    {
-        name: "Flat white",
-        price: 4.8
-    },
-    {
-        name: "Long black",
-        price: 4.5
-    },
-    {
-        name: "Plain biscuit",
-        price: 4.5
-    },
-    {
-        name: "Small cake",
-        price: 7
-    },
-    {
-        name: "Big cake",
-        price: 15.9
-    },
-];
 
 // Handles request for menu page
 app.get("/customer/menu", (req, res) => {
@@ -51,8 +21,61 @@ app.post("/customer/order", (req, res) => {
     res.send(`<h1>start new order</h1>`);
 });
 
-app.listen(8080, () => {
-  console.log("The app is listening on port 8080!");
+const port = 8080 // process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log(`The app is listening on port ${port}!`);
+});
+
+
+// TODO: During tutorial  is it "orders: [orderSchema] or orders: {type: orderSchema}"
+const snackSchema = mongoose.Schema({
+    itemID: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    price: {type: Number, min: 0, required: true},
+    imageURL: {type: String, required: true}
+});
+
+const singleItemOrderSchema = mongoose.Schema({
+    snack: {type: snackSchema, required: true},
+    amount: {type: Number, required: true, min: 1}
+});
+
+const ratingSchema = mongoose.Schema({
+    score: {type: Number, min: 0, max: 5},
+    comment: {type: String}
 });
 
 // need a "[server-name]/vendor" and a "[server-name]/customer"
+// vendor
+const vendorSchema = mongoose.Schema({
+    name: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    open: {type: Boolean, required: true},
+    lat: {type: Number},
+    long: {type: Number}
+});
+
+// TODO: Check during tutorial if we need timeFulfilled, timePickedUp or just discount Applied
+// TODO: how to initialise two schemas that reference each other (?)
+const orderSchema = mongoose.Schema({
+    orderID: {type: String, required: true, unique: true},
+    timeOrdered: {type: Date, default: Date.now},
+    timeFulfilled: {type: Date},
+    timePickedUp: {type: Date},
+    status: {type: String, enum: ['preparing', 'fulfilled', 'completed', 'cancelled'], required: true},
+    discountApplied: {type: Boolean},
+    // customer: customerSchema,
+    vendor: vendorSchema,
+    items: [singleItemOrderSchema],
+    customerRating: ratingSchema
+});
+
+// double check if need to put "unique: false"
+const customerSchema = mongoose.Schema({ 
+    email: {type: String, required: true, unique: true},
+    familyName: {type: String, required: true},
+    givenName: {type: String, required: true},
+    password: {type: String, required: true},
+    orders: [orderSchema]
+});
