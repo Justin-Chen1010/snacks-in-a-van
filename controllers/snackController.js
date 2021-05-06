31afdc9afae3ab21d59e4a417e80826b6d68307a
jonjau <jonjau3q@gmail.com>
2021-05-06 14:41:11 +1000

    can add new items to past orders

--- controllers/snackController.js
+++ controllers/snackController.js
@@ -2,17 +2,25 @@ const mongoose = require("mongoose");
 
 // import snack model
 const Snack = mongoose.model("Snack");
+const Order = mongoose.model("Order");
 
 // get all snacks
 const getAllSnacks = async (req, res) => {
   try {
     const snacks = await Snack.find().lean();
-    res.render("menu", { "snacks": snacks });
+    const currOrder = await Order.findOne({ orderId: req.query.orderId }).lean();
+    res.render("menu", { menu: snacks, order: currOrder});
   } catch (err) {
     res.status(400);
     return res.send("Database query failed");
   }
 };
+
+const getMenu = async (req, res, next) => {
+  const snacks = await Snack.find().lean();
+  req.menu = snacks;
+  next();
+}
 
 // find one snack by their id
 const getOneSnack = async (req, res) => {
@@ -33,6 +41,7 @@ const getOneSnack = async (req, res) => {
 };
 
 module.exports = {
+  getMenu,
   getAllSnacks,
   getOneSnack,
 };
